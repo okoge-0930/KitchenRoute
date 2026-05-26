7b9d0d777c29a0c6ce735d8277df62fdace3bbf3
okoge-0930 <g.tea10vegg@gmail.com>
2026-05-26 15:12:58 +0900

    ロール別ホーム分岐実装

--- KitchenRoute/app/urls.py
+++ KitchenRoute/app/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import LoginView, HomeView
+from .views import (
+    LoginView,
+    HomeView,
+    EducatorHomeView,
+    TraineeHomeView,
+    AdminHomeView,
+)
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
     path("", HomeView.as_view(), name="home"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("educator-home/", EducatorHomeView.as_view(), name="educator_home"),
+    path("trainee-home/", TraineeHomeView.as_view(), name="trainee_home"),
+    path("admin-home/", AdminHomeView.as_view(), name="admin_home"),
 ]
--- KitchenRoute/app/views.py
+++ KitchenRoute/app/views.py
@@ -1,9 +1,36 @@
 from django.contrib.auth.views import LoginView as DjangoLoginView
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 
 class LoginView(DjangoLoginView):
     template_name = "app/login.html"
+
+    def get_success_url(self):
+        user = self.request.user
+
+        if user.role == 0:
+            return "/trainee-home/"
+
+        if user.role == 1:
+            return "/educator-home/"
+
+        if user.role == 2:
+            return "/admin-home/"
+
+        return "/"
+    
+
+class EducatorHomeView(TemplateView):
+    template_name = "app/educator_home.html"
+
+
+class TraineeHomeView(TemplateView):
+    template_name = "app/trainee_home.html"
+
+
+class AdminHomeView(TemplateView):
+    template_name = "app/admin_home.html"
 
 
 class HomeView(TemplateView):
